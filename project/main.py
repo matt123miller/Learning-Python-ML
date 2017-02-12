083a8dd5aebf2638f2ffe9cb4d621e5135a4b458
matt123miller <matt123miller@hotmail.co.uk>
2017-02-12 22:24:48 +0000

    Made a histogram of word lengths. Legit.

--- project/main.py
+++ project/main.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import requests
-#import json
+import matplotlib.pyplot as plt
 
 # Lets make some REST
 
@@ -12,6 +12,20 @@ response = requests.get(apiRoot + "pokemon")
 rawOutput = response.json()
 results = rawOutput["results"]
 
+pokemon = [r for r in results]
 names = [p["name"] for p in results]
-                
+chars = np.array([len(n) for n in names]) 
+
+##print(pokemon)
 print (names)
+print(chars)
+print(x)
+
+# Make my graph axis
+chars = np.sort(chars)          
+x = np.array(range(0, len(chars)))     
+
+#"""
+plt.hist(chars)
+plt.show()
+#"""
