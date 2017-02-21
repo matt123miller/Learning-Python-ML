8a90a6064a03546abd661c845e7e3e252c6f31a4
matt123miller <matt123miller@hotmail.co.uk>
2017-02-21 18:45:03 +0000

    I'm currently working on how to efficiently manipulate that key, which contains roughly 10,000 rows.

--- project/main.py
+++ project/main.py
@@ -10,22 +10,22 @@ import h5py
 #import hdf5
 
 
-mat = io.loadmat('0708 Trial1 TE.mat')
+mat = io.loadmat('0708 Trial3 TE.mat')
 
 data = np.array(mat['data6']).astype(float)
 
+print(np.shape(data))
 
-
-csvFile = open('Participant20.csv', newline='\n')
-
-file = csv.reader(csvFile)
-
-rows = {} 
-i = 0
-
-for row in file:
-    rowdata = np.array(row[1:]).astype(float)
-    rows[row[0]] = rowdata
-    i += 1 # Couldn't work out how to range with the file       
-             
-print(rows.keys())
+#csvFile = open('Participant20.csv', newline='\n')
+#
+#file = csv.reader(csvFile)
+#
+#rows = {} 
+#i = 0
+#
+#for row in file:
+#    rowdata = np.array(row[1:]).astype(float)
+#    rows[row[0]] = rowdata
+#    i += 1 # Couldn't work out how to range with the file       
+#             
+#print(rows.keys())
--- project/matlabImport.py
+++ project/matlabImport.py
@@ -6,6 +6,10 @@ You will need an HDF5 python library to read matlab 7.3 format mat files.
 Because scipy does not supply one, we do not implement the HDF5 / 7.3 interface here.
 
 h5py and hdf5 are both related to importing and reading matlab files
+
+This file isn't actually that useful in the end as I'm interestd in the 'data6' key in the dictionary
+I'm currently working on how to efficiently manipulate that key, which contains roughly 10,000 rows.
+
 """
 
 import numpy as np
@@ -34,7 +38,22 @@ def loadMatlabFile(filename):
     
     return dataX, dataY
     
-dataX, dataY = loadMatlabFile(filename = '0708 Trial1 TE.mat')
+
+def loadMatlabFile(filename, singlekey):
+    matlab = io.loadmat(filename)
+    
+    dataRaw = np.array(matlab[singlekey])
+    
+    dataX = np.array([])
+    dataY = np.array([])
+    
+    # TODO Work on getting the right x y data out for graphing
+    
+    return dataX, dataY
+    
+
+#dataX, dataY = loadMatlabFile(filename = '0708 Trial1 TE.mat')
+dataX, dataY = loadMatlabFile(filename = '0708 Trial1 TE.mat', singlekey = 'data6')
 
 plt.plot(dataX, dataY) # Give it the x and y axis values
 plt.axis([0,10,0,10]) # What is the range of each axis?
