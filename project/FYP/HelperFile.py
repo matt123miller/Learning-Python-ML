f8262ebd421bfa7294551b03a0c8fe1306111b19
Matthew Miller <mmiller@tfspark.com>
2017-03-15 17:14:49 +0000

    Imported this Kernels file that pops up in many tutorials. This one was found from http://tullo.ch/articles/svm-py/ I also created the HelperFile for doing some generic things, mostly around averaging points. Though maybe this should go in the Point class…..don’t care at this point.

---
+++ project/FYP/HelperFile.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import numpy.linalg as la
+from point import Point
+
+class Helper():
+
+    @staticmethod
+    def averagePoints(points):
+        x, y = [], []
+        for p in points:
+            x.append(p.x)
+            y.append(p.y)
+        return Point(sum(x)/len(x),sum(y)/len(y))
+    
+    @staticmethod
+    def splitDataAboveBelowMean(npIn, returnType):
+        above = np.array([])
+        below = np.array([])
+        
+        if returnType == 'm':
+            mean = np.mean(npIn)
+            above = npIn[npIn > mean]
+            below = npIn[npIn < mean]
+        else: #it's for points
+            mean = Helper.averagePoints(npIn).sqrMagnitude()
+#            for p in npIn:
+#                sqrMag = p.sqrMagnitude()
+#                if sqrMag > mean:
+#                    above = np.append(above, p)
+#                else:
+#                    below = np.append(below, p)
+            
+            above = [p for p in npIn if p.sqrMagnitude() > mean]
+            below = [p for p in npIn if p.sqrMagnitude() < mean]
+        return above, below
